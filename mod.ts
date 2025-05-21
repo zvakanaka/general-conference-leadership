fb6c49977118c2cd6c89834e42d0a8fc32edd6e2
Adam <8365885+zvakanaka@users.noreply.github.com>
2025-05-21 15:12:08 -0600

    docs again

--- mod.ts
+++ mod.ts
@@ -2,4 +2,5 @@
 import leadership from "./data/derived/general-conference-leadership.json" with { type: "json" };
 import type GeneralConferenceLeadership from "./types.d.ts";
 
+/** The leadership data as an array of GeneralConferenceLeadership objects. */
 export default leadership as GeneralConferenceLeadership[];
